5df1ded9670e098dabed44177c469562b033c8ec
jeff <zhangjianfen@outlook.com>
2016-06-01 14:58:49 +0800

    [Added] update status in svg.

---
+++ src/main/java/com/dxw/flfs/app/TestApp.java
@@ -0,0 +1,18 @@
+package com.dxw.flfs.app;/**
+ * Created by zhang on 2016-06-01.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/main/java/com/dxw/flfs/ui/controllers/SvgController.java
+++ src/main/java/com/dxw/flfs/ui/controllers/SvgController.java
@@ -1,14 +1,10 @@
 package com.dxw.flfs.ui.controllers;
 
-import com.dxw.flfs.communication.PlcDelegate;
-import com.dxw.flfs.communication.PlcDelegateFactory;
-import com.dxw.flfs.communication.PlcModel;
-import com.dxw.flfs.communication.PlcModelField;
+import com.dxw.flfs.communication.*;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 /**
  * Created by zhang on 2016-05-19.
@@ -25,83 +21,92 @@ public class SvgController {
         String url = getClass().getResource("/svg/flfs.html").toExternalForm();
         engine.load(url);
 
-        //addPlcListener();
+        addPlcListener();
 
     }
 
     private void addPlcListener() {
         PlcDelegate delegate = PlcDelegateFactory.getPlcDelegate();
-        delegate.addModelChangedListener(event -> {
-            long field = event.getField();
-            PlcModel model = event.getModel();
+        delegate.addModelChangedListener(event -> Platform.runLater(() -> updateStatus(event)));
+    }
 
-            if (field == PlcModelField.SYSTEM_STATUS) {
-                Short status = model.getSystemStatus();
-                WebEngine engine = webView.getEngine();
-                Document document = engine.getDocument();
-                Element ele = document.getElementById("textSystemStatus");
+    private void updateStatus(PlcModelChangedEvent event) {
+        long field = event.getField();
+        PlcModel model = event.getModel();
+        WebEngine engine = webView.getEngine();
 
-                switch (status) {
-                    case 1:
-                        ele.setTextContent("停机");
-                        break;
-                    case 2:
-                        ele.setTextContent("做料");
-                        break;
-                    case 3:
-                        ele.setTextContent("清洗");
-                        break;
-                    case 4:
-                        ele.setTextContent("紧停");
-                        break;
-                    case 5:
-                        ele.setTextContent("冷启动");
-                        break;
-                }
+        if (field == PlcModelField.SYSTEM_STATUS) {
+            Short status = model.getSystemStatus();
 
-            } /*else if (field == PlcModelField.FERMENT_BARREL_STATUS) {
-                boolean[] data = model.getFermentBarrelStatus();
-                String status = "";
-                for (int i = 0; i < Math.min(data.length,7); i++) {
-                    status += ((data[i]) ? "满" : "空") + " ";
-                }
-                Element ele = document.getElementById("textFermentBarrelStatus");
-                ele.setTextContent(status);
+            String text = "";
+            switch (status) {
+                case 1:
+                    text = "停机";
+                    break;
+                case 2:
+                    text = "做料";
+                    break;
+                case 3:
+                    text = "清洗";
+                    break;
+                case 4:
+                    text = "紧停";
+                    break;
+                case 5:
+                    text = "冷启动";
+                    break;
+            }
+            engine.executeScript(
+                    String.format("document.getElementById(\"textSystemStatus\").textContent = \"%s\"",
+                            text)
+            );
 
-            } *//*else if (field == PlcModelField.MATERIAL_TOWER_ALARM) {
-                Boolean lowAlarm = model.getMaterialTowerLowAlarm();
-                Boolean emptyAlarm = model.getMaterialTowerEmptyAlarm();
+        } else if (field == PlcModelField.FERMENT_BARREL_STATUS) {
+            boolean[] data = model.getFermentBarrelStatus();
+            String text = "";
+            for (int i = 0; i < Math.min(data.length, 7); i++) {
+                text += ((data[i]) ? "满" : "空") + " ";
+            }
+            engine.executeScript(
+                    String.format("document.getElementById(\"textFermentBarrelStatus\").textContent = \"%s\"",
+                            text)
+            );
 
-                if (lowAlarm) {
-                    lblMaterialTowerLow.setIcon(iconAlert);
-                    lblMaterialTowerLow.setText("");
-                } else {
-                    lblMaterialTowerLow.setIcon(null);
-                    lblMaterialTowerLow.setText("正常");
-                }
+        }else if (field == PlcModelField.MIXING_BARREL_STATUS) {
+            Short status = model.getMixingBarrelStatus();
+            String text = status == 0 ? "空闲" : "运行";
 
-                if (emptyAlarm) {
-                    lblMaterialTowerEmpty.setIcon(iconAlert);
-                    lblMaterialTowerEmpty.setText("");
-                } else {
-                    lblMaterialTowerEmpty.setIcon(null);
-                    lblMaterialTowerEmpty.setText("正常");
-                }
-            } else if (field == PlcModelField.FERMENT_BARREL_IN_OUT) {
+            engine.executeScript(
+                    String.format("document.getElementById(\"textMixingBarrelStatus\").textContent = \"%s\"",
+                            text)
+            );
+        } /*else if (field == PlcModelField.MATERIAL_TOWER_ALARM) {
+            Boolean lowAlarm = model.getMaterialTowerLowAlarm();
+            Boolean emptyAlarm = model.getMaterialTowerEmptyAlarm();
+
+            if (lowAlarm) {
+                lblMaterialTowerLow.setIcon(iconAlert);
+                lblMaterialTowerLow.setText("");
+            } else {
+                lblMaterialTowerLow.setIcon(null);
+                lblMaterialTowerLow.setText("正常");
+            }
+
+            if (emptyAlarm) {
+                lblMaterialTowerEmpty.setIcon(iconAlert);
+                lblMaterialTowerEmpty.setText("");
+            } else {
+                lblMaterialTowerEmpty.setIcon(null);
+                lblMaterialTowerEmpty.setText("正常");
+            }
+        } else if (field == PlcModelField.FERMENT_BARREL_IN_OUT) {
                 short in = model.getFermentBarrelInNo();
                 short out = model.getFermentBarrelOutNo();
                 lblFermentBarrelIn.setText(Short.toString(in));
                 lblFermentBarrelOut.setText(Short.toString(out));
-            } else if (field == PlcModelField.MIXING_BARREL_STATUS) {
-                Short status = model.getMixingBarrelStatus();
-                lblMixingBarrelStatus.setText(status == 0 ? "空闲" : "运行");
-            } else if (field == PlcModelField.PH_VALUE) {
+            }  else if (field == PlcModelField.PH_VALUE) {
                 float ph = model.getPh();
                 lblPh.setText(Float.toString(ph));
             }*/
-            //canvas.repaint();
-            //canvas.getCanvasGraphicsNode().fireGraphicsNodeChangeCompleted();
-
-        });
     }
 }
